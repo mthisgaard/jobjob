23f6e9de642b2922132c86de4294c04015b3b916
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-23 11:11:41 +0900

    created job and task seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-puts 'Removing all users...'
+require 'faker'
+
+puts 'Removing all users, jobs and tasks...'
 User.destroy_all
 
 puts 'Creating users...'
@@ -8,13 +10,36 @@ User.create!(email: 'andre@me.com', first_name: 'andre', last_name: '928', passw
 User.create!(email: 'nikki@me.com', first_name: 'nikki', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/94282878?v=4')
 puts "created #{User.count} users"
 
+puts 'Creating jobs...'
 User.all.each do |user|
   rand(6..9).times do
     Job.create!(
-      title:
-      company:
-      deadline:
-      url:
+      title: Faker::Job.title,
+      company: Faker::Company.name,
+      deadline: Date.today + rand(20),
+      url: 'https://www.linkedin.com/jobs/'
+      user: user
     )
   end
 end
+puts "created #{Job.count} jobs"
+
+puts 'Creating tasks...'
+Job.all.each do |job|
+    Task.create!(
+      title: 'Research the company'
+      job: job
+    )
+    Task.create!(
+      title: 'Write cover letter'
+      job: job
+    )
+    Task.create!(
+      title: 'Call contact person at the company'
+      job: job
+    )
+  end
+end
+puts "created #{Task.count} tasks"
+
+
