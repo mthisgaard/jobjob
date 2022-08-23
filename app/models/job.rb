cc87d52236446c654cc626e2b397d0558e958eee
Andre Bauer <andrebauer@live.com>
2022-08-23 11:58:04 +0900

    the controllers and views to go

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,6 @@ class Job < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :title, :company, :url, presence: true
   enum status: { pending: 0, applied: 1, interview: 2, rejected: 3, offer: 4 }
+  has_one_attached :cv
+  has_one_attached :cover_letter
 end
