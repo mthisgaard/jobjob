2c3db06818f1ce48d5e774305c38157639871624
Adam Hemming <hemming.dev@gmail.com>
2022-08-24 12:34:04 +0900

    get job id in js

--- app/javascript/controllers/edit_notes_controller.js
+++ app/javascript/controllers/edit_notes_controller.js
@@ -2,6 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-notes"
 export default class extends Controller {
+  static targets = ["note"]
+
   connect() {
+    // console.log("edit notes controller")
+    // console.log(this.noteTarget.innerHTML)
+    this.noteTargets.forEach(note => {
+      console.log(note.dataset.jobid)
+    });
+  }
+
+  updateNote() {
+    // console.log(this.noteTarget.innerHTML)
+    // console.log(this.currentTarget.dataset.jobId)
   }
 }
