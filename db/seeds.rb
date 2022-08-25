b7f0833c2f70653ac90b992a39052b356e08b474
Adam Hemming <hemming.dev@gmail.com>
2022-08-25 16:55:14 +0900

    make demo account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ User.destroy_all
 
 puts 'Creating users...'
 User.create!(email: 'adam@me.com', first_name: 'adam', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/103736492?v=4')
-User.create!(email: 'malene@me.com', first_name: 'malene', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/107089457?v=4')
 User.create!(email: 'andre@me.com', first_name: 'andre', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/102734981?v=4')
 User.create!(email: 'nikki@me.com', first_name: 'nikki', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/94282878?v=4')
 puts "created #{User.count} users"
@@ -54,3 +53,7 @@ Job.all.each do |job|
   task_3.save!
 end
 puts "created #{Task.count} tasks"
+
+puts "creating account for demo"
+
+User.create!(email: 'malene@me.com', first_name: 'malene', last_name: '928', password: '123123', avatar: 'https://avatars.githubusercontent.com/u/107089457?v=4')
