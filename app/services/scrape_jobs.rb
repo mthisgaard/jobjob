f896feb69935fa5dfbae36e86081e2ac77c47590
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-29 13:34:18 +0900

    scraping job card info

--- app/services/scrape_jobs.rb
+++ app/services/scrape_jobs.rb
@@ -2,19 +2,15 @@ require "open-uri"
 require "nokogiri"
 
 class ScrapeJobs
-  def initialize(search_term)
-    @search_term = search_term
+  def initialize(keyword)
+    @keyword = keyword
   end
 
   def call
-    url = "https://japan-dev.com/jobs?query=#{@search_term}"
-
-    html_file = URI.open(url).read
-    html_doc = Nokogiri::HTML(html_file)
-
-    html_doc.search(".job-item__main-data").each do |element|
-      puts element.text.strip
-      puts element.attribute("href").value
+    url = "https://japan-dev.com/jobs?query=#{@keyword}"
+    html_doc = Nokogiri::HTML(URI.open(url).read)
+    html_doc.search(".job-item__main-data").map do |job_card|
+      job_card.search(".job-item__title-box").attribute("href").value
     end
   end
 end
