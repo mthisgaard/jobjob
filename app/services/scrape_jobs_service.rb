39ba13d91464afb9e68d714506a0b6676efea434
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-30 10:58:34 +0900

    conditional to only show suggestions not added to jobs

--- app/services/scrape_jobs_service.rb
+++ app/services/scrape_jobs_service.rb
@@ -2,15 +2,11 @@ require "open-uri"
 require "nokogiri"
 
 class ScrapeJobsService
-  # def initialize(keyword)
-  #   @keyword = keyword
-  # end
-
   def call
     url = "https://www.tokyodev.com/jobs"
     html_file = URI.open(url).read
     html_doc = Nokogiri::HTML(html_file)
-    html_doc.search(".jobs-list__item").first(4).map do |job_card|
+    html_doc.search(".jobs-list__item").first(15).map do |job_card|
       {
         title: job_card.search(".job-title .link-reset").first.text.strip,
         company: job_card.search(".company-title").text.strip,
