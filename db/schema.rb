55e46e716ac33d9386ac63a300d34a2c35233d83
Adam Hemming <hemming.dev@gmail.com>
2022-08-30 11:24:16 +0900

    stealing nikki's code

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_08_29_012533) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_004522) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
