a7154762940b49b1125a11d57c7c93486d68a877
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-30 13:36:23 +0900

    fixed grid and spacing

--- db/schema.rb
+++ db/schema.rb
@@ -127,11 +127,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_012533) do
     t.string "first_name"
     t.string "last_name"
     t.string "avatar", default: "avatar.png"
-<<<<<<< HEAD
     t.integer "sash_id"
     t.integer "level", default: 0
-=======
->>>>>>> master
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
