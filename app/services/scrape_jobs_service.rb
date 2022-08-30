92704f682d78f94c9a6c6612c672616377d35a6b
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-30 11:37:00 +0900

    render cards for first 8 suggestions

--- app/services/scrape_jobs_service.rb
+++ app/services/scrape_jobs_service.rb
@@ -6,7 +6,7 @@ class ScrapeJobsService
     url = "https://www.tokyodev.com/jobs"
     html_file = URI.open(url).read
     html_doc = Nokogiri::HTML(html_file)
-    html_doc.search(".jobs-list__item").first(15).map do |job_card|
+    html_doc.search(".jobs-list__item").first(16).map do |job_card|
       {
         title: job_card.search(".job-title .link-reset").first.text.strip,
         company: job_card.search(".company-title").text.strip,
