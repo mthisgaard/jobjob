982ea178621ef59f97b752b7f9f266e90e07429c
Adam Hemming <hemming.dev@gmail.com>
2022-08-31 17:03:13 +0900

    create notification controller

---
+++ app/javascript/controllers/show_notification_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="show-notification"
+export default class extends Controller {
+  connect() {
+    console.log("notification")
+  }
+
+  display() {
+
+  }
+}
