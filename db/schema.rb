39fcf2ab780a56666ccba01add1c89dfe9d55e7d
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-09-01 17:55:19 +0900

    add content column to emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_081113) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_085318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_081113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
+    t.text "content"
     t.index ["job_id"], name: "index_emails_on_job_id"
     t.index ["user_id"], name: "index_emails_on_user_id"
   end
