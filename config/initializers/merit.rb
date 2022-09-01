db46d917245beb5119fe70601c64d476bfae1f96
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-09-01 12:04:38 +0900

    finished badge styles

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -22,42 +22,42 @@ Rails.application.reloader.to_prepare do
     id: 1,
     name: "JobJob Pro",
     description: "Completed 3 tasks in one day",
-    custom_fields: "badge1_static.png"
+    custom_fields: { icon: "badge1_static.png", badge: "badge1.gif" }
   }, {
     id: 2,
     name: 'Jobberwocky',
     description: "Completed 5 tasks in one day",
-    custom_fields: "badge2_static.png"
+    custom_fields: { icon: "badge2_static.png", badge: "badge2.gif" }
   }, {
     id: 3,
     name: 'Numero Jobbo',
     description: "Applied to your first job",
-    custom_fields: "badge3_static.png"
+    custom_fields: { icon: "badge3_static.png", badge: "badge3.gif" }
   }, {
     id: 4,
     name: 'Good JobJob',
     description: "Applied to 3 jobs",
-    custom_fields: "badge4_static.png"
+    custom_fields: { icon: "badge4_static.png", badge: "badge4.gif" }
   }, {
     id: 5,
     name: 'Steve Jobs',
     description: "Applied to 5 jobs",
-    custom_fields: "badge5_static.png"
+    custom_fields: { icon: "badge5_static.png", badge: "badge5.gif" }
   }, {
     id: 6,
     name: 'Job Magnet',
     description: "Get 3 interviews",
-    custom_fields: "badge6_static.png"
+    custom_fields: { icon: "badge6_static.png", badge: "badge6.gif" }
   }, {
     id: 7,
     name: 'Jobbapalooza',
     description: "Get 5 interviews",
-    custom_fields: "badge7_static.png"
+    custom_fields: { icon: "badge7_static.png", badge: "badge7.gif" }
   }, {
     id: 8,
-    name: 'The Jobfather',
+    name: 'ConJOBulations!',
     description: "Get a job offer",
-    custom_fields: "badge8_static.png"
+    custom_fields: { icon: "badge8_static.png", badge: "badge8.gif" }
   }].each do |attrs|
     Merit::Badge.create! attrs
   end
